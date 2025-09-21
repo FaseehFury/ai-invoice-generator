a17814db0e58b3155af4531823559f41c9752ccb
FaseehFury <faseehali506@gmail.com>
2025-09-21 17:25:24 +0500

    finish with mongodb connection

---
+++ backend/server.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db");
+
+const app = express();
+
+//Middleware to handle cors
+
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  })
+);
+
+//connect Database
+connectDB();
+
+//Middleware
+app.use(express.json());
+
+//Routes Here
+
+//Start Server
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running port ${PORT}`);
+});
