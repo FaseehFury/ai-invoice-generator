c8df73567c3efc11a9c0af2c5b1887d7bb6ad93a
FaseehFury <faseehali506@gmail.com>
2025-09-21 16:43:24 +0500

    with footer our landing page UI is completed

--- frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
+++ frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
@@ -1,6 +1,7 @@
 import Header from "../../components/landing/Header";
 import Faqs from "./Faqs";
 import Features from "./Features";
+import Footer from "./Footer";
 import Hero from "./Hero";
 import Testimonials from "./Testimonials";
 
@@ -8,11 +9,14 @@ const LandingPage = () => {
   return (
     <div className="bg-[#ffffff] text-gray-600">
       <Header />
-      <main className="mb-[100vh]">
+      {/* when we was adding components to main we added mb-[100vh] to main to avoid footer overlapping with last component */}
+      {/* <main className="mb-[100vh]"> */}
+      <main>
         <Hero />
         <Features />
         <Testimonials />
         <Faqs />
+        <Footer />
       </main>
     </div>
   );
