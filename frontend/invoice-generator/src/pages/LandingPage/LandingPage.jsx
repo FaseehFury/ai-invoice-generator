108200c845dbf40857d96d21c8fc59f8e2a4307d
FaseehFury <faseehali506@gmail.com>
2025-09-21 14:10:46 +0500

    features componenet ui completed

--- frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
+++ frontend/invoice-generator/src/pages/LandingPage/LandingPage.jsx
@@ -1,11 +1,12 @@
 import Header from "../../components/landing/Header";
+import Features from "./Features";
 import Hero from "./Hero";
 
 const LandingPage = () => {
   return (
     <div className="bg-[#ffffff] text-gray-600">
       <Header />
-      <main>
+      <main className="mb-[100vh]">
         <Hero />
         <Features />
       </main>
---
+++ frontend/invoice-generator/src/utils/data.js
@@ -0,0 +1,28 @@
+import { BarChart, FileText, Mail, Sparkles } from "lucide-react";
+
+export const FEATURES = [
+  {
+    icon: Sparkles,
+    title: "AI Invoice Creation",
+    description:
+      "Create polished invoices in seconds with AI assistance, saving time and reducing manual work.",
+  },
+  {
+    icon: BarChart,
+    title: "AI-Powered Dashboard",
+    description:
+      "Visualize your business growth with real-time charts and analytics that highlight key metrics.",
+  },
+  {
+    icon: Mail,
+    title: "Smart Reminders",
+    description:
+      "Automatically send friendly payment reminders to clients via email so you never miss a due date.",
+  },
+  {
+    icon: FileText,
+    title: "Easy Invoice Management",
+    description:
+      "Organize, edit, and track all invoices in one place with a simple and secure management system.",
+  },
+];
