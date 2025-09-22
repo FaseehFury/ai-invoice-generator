a107e0c16e81159a6f29a3946db45361a84b6dab
FaseehFury <faseehali506@gmail.com>
2025-09-22 22:51:35 +0500

    finsih with our backend if any modification need we will do later and with this we decide our apiPaths and make axios instances for that paths rather than hardcoaded paths everywhere

---
+++ frontend/invoice-generator/src/utils/axiosInstance.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+import { BASE_URL } from "./apiPaths";
+
+const axiosInstance = axios.create({
+  baseURL: BASE_URL,
+  timeout: 8000,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+});
+
+//Request Interceptor
+
+axiosInstance.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("token");
+    if (accessToken) {
+      config.headers.Authorization = `Beare ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+//Response Interceptor
+
+axiosInstance.interceptors.request.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response) {
+      if (error.response.status === 500) {
+        console.log("Server error. Please try again later.");
+      }
+    } else if (error.code === "ECONNABORTED") {
+      console.error("Request timeout. please try again.");
+    }
+  }
+);
+
+export default axiosInstance;
